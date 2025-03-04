9b6d9a7bffa61f6992c975ac5bf1da178e62c5c2
cisse17 <119404406+cisse17@users.noreply.github.com>
2025-03-04 16:01:17 +0100

    comment lire une vidéo avec opencv

--- read.py
+++ read.py
@@ -1,8 +1,24 @@
 import cv2 as cv
 
-img  = cv.imread('Photos/cat.jpg')
+"""Lire une Photo"""
+# img  = cv.imread('Photos/cat.jpg')
 
-cv.imshow('Chat', img)
+# cv.imshow('Chat', img)
+
+
+"""Lire une Vidéo"""
+
+capture = cv.VideoCapture('Videos/growth_event.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'): # press lettre d pour stopper la vidéo
+        break
+
+capture.release()
+cv.destroyAllWindows()
 
 
 cv.waitKey(0)
